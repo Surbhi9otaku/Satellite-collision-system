230b7688bd4b8458c4f53d45a2516d00463ce2af
Surbhi Kumari <rpsurbhi955@gmail.com>
2026-03-14 22:54:34 +0530

    initial commit

---
+++ frontend/script.js
@@ -0,0 +1,11 @@
+document.getElementById("checkBtn").onclick = function(){
+
+document.querySelector("#result").innerHTML =
+`
+<h3>Result</h3>
+<p>Collision Risk: Checking...</p>
+<p>Suggested Maneuver: Calculating...</p>
+<p>Fuel Required: Calculating...</p>
+`
+
+}
